789231961daf1d105db67fb241c1d892a6fb7940
jubianchi <contact@jubianchi.fr>
2012-03-25 16:27:26 +0200

    Dump commitgit add res

---
+++ PickAndDestroy/src/CharacterRose.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+import pulpcore.Stage;
+import pulpcore.math.Rect;
+import pulpcore.sound.Playback;
+import pulpcore.sound.Sound;
+
+
+public class CharacterRose extends Character
+{	
+	public CharacterRose ( EntityManager entityManager )
+	{		
+		super ( entityManager );
+		
+		this.sprite = "ecureuil_rose";
+	}
+}
