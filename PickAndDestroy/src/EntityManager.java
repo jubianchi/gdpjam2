178ccd2fc91aee0543c7075f81f7b763baa9e3a3
jubianchi <contact@jubianchi.fr>
2012-03-25 12:00:10 +0200

    Revert vers le bon EntityManager

--- PickAndDestroy/src/EntityManager.java
+++ PickAndDestroy/src/EntityManager.java
@@ -1,10 +1,8 @@
 import java.util.ArrayList;
 import java.util.List;
 
-import pulpcore.math.Rect;
 import pulpcore.scene.Scene2D;
 import pulpcore.sprite.Group;
-import pulpcore.sprite.ImageSprite;
 import pulpcore.sprite.Sprite;
 
 
@@ -19,106 +17,52 @@ public class EntityManager
 	{
 		group = new Group ();
 
-
-		
-		//sprites barre du haut
-		addSprite ( 5, 2, 1,1, "1x1ObjP2.png", -0, -10 );
-		addSprite ( 6, 2, 1,1, "PtTableHz.png", 0,-5 );
-		addSprite ( 7, 2, 1,1, "PtTableVt.png", 0,-5 );
-		addSprite ( 8, 2, 1,1, "PtTableHz.png", 0,-5 );
-		addSprite ( 9, 2, 1,1, "1x1ObjP2.png", -0, -10 );
-		
-		// sprites angle haut gauche
-		addSprite ( 3, 5, 1,1, "1x1ObjP2.png", -0,-10 );
-		addSprite ( 3, 6, 1,1, "PtTableHz.png", 0,-5 );
-		addSprite ( 4, 5, 1,1, "PtTableVt.png", 0,-5 );
-		addSprite ( 5, 5, 1,1, "PtTableHz.png", 0,-5 );
-
-		// sprites angle bas gauche
-		addSprite ( 3, 8, 1,1, "PtTableHz.png", 0,-5 );
-		addSprite ( 3, 9, 1,1, "1x1ObjP2.png", -0, -10 );
-		addSprite ( 4, 9, 1,1, "PtTableVt.png", 0,-5 );
-		addSprite ( 5, 9, 1,1, "PtTableHz.png", 0,-5 );
-		
-		// sprites angle haut droite
-		addSprite ( 9, 5, 1,1, "PtTableHz.png", 0,-5 );
-		addSprite ( 10, 5, 1,1, "PtTableVt.png", 0,-5 );
-		addSprite ( 11, 5, 1,1, "1x1ObjP2.png", -0,-10 );
-		addSprite ( 11, 6, 1,1, "PtTableHz.png", 0,-5 );
-
-		// sprites angle bas droite
-		
-		addSprite ( 11, 8, 1,1, "PtTableHz.png", 0,-5 );
-		addSprite ( 9, 9, 1,1, "PtTableVt.png", 0,-5 );
-		addSprite ( 10, 9, 1,1, "PtTableHz.png", 0,-5 );
-		addSprite ( 11, 9, 1,1, "1x1ObjP2.png", -0,-10 );
-		
-		//sprites barre du bas
-		addSprite ( 5, 12, 1,1, "1x1ObjP2.png", -0,-10 );
-		addSprite ( 6, 12, 1,1, "PtTableHz.png", 0,-5 );
-		addSprite ( 7, 12, 1,1, "PtTableVt.png", 0,-5 );
-		addSprite ( 8, 12, 1,1, "PtTableHz.png", 0,-5 );
-		addSprite ( 9, 12, 1,1, "1x1ObjP2.png", -0,-10 );			
-		
-		// sprites 2x2
-		addSprite ( 2, 2, 2,2, "2x2Obj_01.png", 0, 0 );
-		addSprite ( 11, 11, 2,2, "2x2Obj_01.png", 0,-5 );
-		addSprite ( 2, 11, 2,2, "2x2Obj_01.png", 0,-5 );
-		addSprite ( 11, 2, 2,2, "2x2Obj_01.png", 0,-5 );
-		// addSprite ( 200, 20 );
-		// addSprite ( 200, 2100 );
-
 		scene.add(group);
-
-		/*
-		debugImage = new CoreImage ( 800, 600 );
-		CoreGraphics graphics = debugImage.createGraphics ();
-		graphics.setColor ( 0xFF00FF00 );
-		// graphics.setAlpha ( 0x00 );
-		graphics.fillRect ( 0,0,800,600 );
-		// graphics.clear ();
-		graphics.drawLine ( 0,0, 800, 600 );
-		debugImageSprite = new ImageSprite(debugImage,0,0);
-		debugGroup = new Group();
-		debugGroup.setBlendMode ( BlendMode.Add() );
-		debugGroup.add ( debugImageSprite );
-		scene.add ( debugGroup );
-		*/
 	}
 	
-	private void addSprite ( int x, int y, int sx ,int sy, String name, int tx, int ty )
-	{
-		Entity entity = new Entity
-		(
-			name,
-			TilemapManager.tileXToPixel(sx),
-			TilemapManager.tileYToPixel(sy)
-		);
-		getEntities ().add ( entity );
-		
-		// entity.getSprite ().getImage ().width
-		// entity.setTranslation ( tx, ty );
-		
-		int px = TilemapManager.tileXToPixel(x);
-		int py = TilemapManager.tileYToPixel(y);
-		entity.setLocation ( px, py );
-
-		double anchorX = -(double)tx / (double)entity.getSprite ().getImage ().getWidth ();
-		double anchorY = -(double)ty / (double)entity.getSprite ().getImage ().getHeight ();
-		entity.getSprite().setAnchor ( 0.5 + anchorX, 0.5 + anchorY );
-
-		group.add(entity.getSprite());
-	}
+	private int totalElapsedTime = 0;
 	
 	public void update(int elapsedTime)
     {
+		// dumpGroup ();
+		
+		int i = 1;
+		while ( i < group.size() )
+        {
+			Sprite sprite = group.get ( i );
+			
+			int j = i-1;
+			while ( j >= 0 )
+			{
+				Sprite previous = group.get ( j );
+				
+				int y = sprite.y.getAsInt ();
+				int previousY = previous.y.getAsInt ();
+				
+				if ( y < previousY )
+				{
+					// System.out.println ( "Moving " + y + " < " + previousY );
+					group.moveDown ( sprite );
+					// dumpGroup ();
+				}
+				else
+					break;
+				
+				j--;
+			}
+			i++;
+        }
+		
+		// dumpGroup ();
+		
+		/*
 		int i = 0;
 		while ( i < group.size() )
         {
 			Sprite element = group.get ( i );
 			Sprite lastElement = group.get ( group.size () - 1 );
 			int elementY =element.y.getAsInt ();
-			int lastElementY =lastElement.y.getAsInt ();
+			int lastElementY = lastElement.y.getAsInt ();
 			if ( elementY > lastElementY )
 			{
 				group.moveToTop ( element );
@@ -128,7 +72,21 @@ public class EntityManager
 				i++;
 			}
         }
+		*/
     }
+
+	private void dumpGroup()
+	{
+		System.out.println("---");
+		int i;
+		i = 0;
+		while ( i < group.size() )
+        {
+			Sprite element = group.get ( i );
+			System.out.println("At " + i + " y = " + element.y.getAsInt () );
+			i++;
+        }
+	}
 
 	public void addEntity(Entity entity)
 	{
